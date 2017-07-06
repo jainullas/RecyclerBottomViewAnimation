4805708b187f07970ed974004fc508f5b0a54963
UllasJain <ullasjain21@gmail.com>
2017-07-07 00:07:16 +0530

    bottom view animation done

--- app/src/main/java/com/example/ullasjain/recyclerbottomviewanimation/MainActivity.java
+++ app/src/main/java/com/example/ullasjain/recyclerbottomviewanimation/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
